dde6b1f89c8a5757c75d60da79c9ff61c769f17d
Yohei Nishikubo <yohei@linker.plus>
2023-01-11 15:58:31 +0900

    change kms library

--- src/provider.ts
+++ src/provider.ts
@@ -2,6 +2,7 @@ import {
   createSignature,
   getEthAddressFromKMS,
 } from "@rumblefishdev/eth-signer-kms";
+
 import { KMS } from "aws-sdk";
 import { BigNumber, utils } from "ethers";
 import { keccak256 } from "ethers/lib/utils";
@@ -12,6 +13,14 @@ import { EIP1193Provider, RequestArguments } from "hardhat/types";
 
 import { toHexString } from "./utils";
 
+export interface GcpKmsSignerCredentials {
+  projectId: string;
+  locationId: string;
+  keyRingId: string;
+  keyId: string;
+  keyVersion: string;
+}
+
 export class KMSSigner extends ProviderWrapperWithChainId {
   public kmsKeyId: string;
   public kmsInstance: KMS;
@@ -21,6 +30,93 @@ export class KMSSigner extends ProviderWrapperWithChainId {
     super(provider);
     this.kmsKeyId = kmsKeyId;
     this.kmsInstance = new KMS();
+  }
+
+  public async request(args: RequestArguments): Promise<unknown> {
+    const method = args.method;
+    const params = this._getParams(args);
+    const sender = await this._getSender();
+    if (method === "eth_sendTransaction") {
+      const [txRequest] = validateParams(params, rpcTransactionRequest);
+      const tx = await utils.resolveProperties(txRequest);
+      const nonce = tx.nonce ?? (await this._getNonce(sender));
+      const baseTx: utils.UnsignedTransaction = {
+        chainId: (await this._getChainId()) || undefined,
+        data: tx.data,
+        gasLimit: tx.gas,
+        gasPrice: tx.gasPrice,
+        nonce: Number(nonce),
+        type: 2,
+        to: toHexString(tx.to),
+        value: tx.value,
+        maxFeePerGas: tx.maxFeePerGas?.toString(),
+        maxPriorityFeePerGas: tx.maxPriorityFeePerGas?.toString(),
+      };
+
+      if (
+        baseTx.maxFeePerGas === undefined &&
+        baseTx.maxPriorityFeePerGas === undefined
+      ) {
+        baseTx.type = 0;
+        delete baseTx.maxFeePerGas;
+        delete baseTx.maxPriorityFeePerGas;
+      }
+
+      const unsignedTx = utils.serializeTransaction(baseTx);
+      const hash = keccak256(utils.arrayify(unsignedTx));
+      const sig = await createSignature({
+        kmsInstance: this.kmsInstance,
+        keyId: this.kmsKeyId,
+        message: hash,
+        address: sender,
+      });
+
+      const rawTx = utils.serializeTransaction(baseTx, sig);
+
+      return this._wrappedProvider.request({
+        method: "eth_sendRawTransaction",
+        params: [rawTx],
+      });
+    } else if (
+      args.method === "eth_accounts" ||
+      args.method === "eth_requestAccounts"
+    ) {
+      return [sender];
+    }
+
+    return this._wrappedProvider.request(args);
+  }
+
+  private async _getSender(): Promise<string> {
+    if (!this.ethAddress) {
+      this.ethAddress = await getEthAddressFromKMS({
+        keyId: this.kmsKeyId,
+        kmsInstance: this.kmsInstance,
+      });
+    }
+    return this.ethAddress;
+  }
+
+  private async _getNonce(address: string): Promise<number> {
+    const response = await this._wrappedProvider.request({
+      method: "eth_getTransactionCount",
+      params: [address, "pending"],
+    });
+
+    return BigNumber.from(response).toNumber();
+  }
+}
+
+export class GCPSigner extends ProviderWrapperWithChainId {
+  private kmsCredentials: GcpKmsSignerCredentials;
+  public ethAddress?: string;
+
+  constructor(
+    provider: EIP1193Provider,
+    kmsCredentials: GcpKmsSignerCredentials
+  ) {
+    super(provider);
+    this.kmsCredentials = kmsCredentials;
   }
 
   public async request(args: RequestArguments): Promise<unknown> {
